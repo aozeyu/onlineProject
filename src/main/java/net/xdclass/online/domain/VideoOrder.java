0b0183e483d0edc9f0f9918ed9bcabdff4c6e0b9
yaozeyu3 <yaozeyu3@jd.com>
2022-04-26 12:35:21 +0800

    提交类

---
+++ src/main/java/net/xdclass/online/domain/VideoOrder.java
@@ -0,0 +1,11 @@
+package net.xdclass.online.domain;
+
+/**
+ * @program: online
+ * @description: 订单类
+ * @packagename: net.xdclass.online.domain
+ * @author: 姚泽宇
+ * @date: 2022-04-26 12:34
+ **/
+public class VideoOrder {
+}
