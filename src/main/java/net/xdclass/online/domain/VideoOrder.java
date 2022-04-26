2e0e88b0f03592026609df847b649bbb04d82751
yaozeyu3 <yaozeyu3@jd.com>
2022-04-26 12:39:21 +0800

    提交类

--- src/main/java/net/xdclass/online/domain/VideoOrder.java
+++ src/main/java/net/xdclass/online/domain/VideoOrder.java
@@ -1,5 +1,7 @@
 package net.xdclass.online.domain;
 
+import java.util.Date;
+
 /**
  * @program: online
  * @description: 订单类
@@ -8,4 +10,90 @@ package net.xdclass.online.domain;
  * @date: 2022-04-26 12:34
  **/
 public class VideoOrder {
+    private Integer id;
+    private String outTradeNo;
+    private Date createTime;
+    private Integer totalFee;
+    private Integer videoId;
+    private String videoTitle;
+    private String videoImg;
+    private Integer userId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOutTradeNo() {
+        return outTradeNo;
+    }
+
+    public void setOutTradeNo(String outTradeNo) {
+        this.outTradeNo = outTradeNo;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoOrder{" +
+                "id=" + id +
+                ", outTradeNo='" + outTradeNo + '\'' +
+                ", createTime=" + createTime +
+                ", totalFee=" + totalFee +
+                ", videoId=" + videoId +
+                ", videoTitle='" + videoTitle + '\'' +
+                ", videoImg='" + videoImg + '\'' +
+                ", userId=" + userId +
+                '}';
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getTotalFee() {
+        return totalFee;
+    }
+
+    public void setTotalFee(Integer totalFee) {
+        this.totalFee = totalFee;
+    }
+
+    public Integer getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(Integer videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getVideoTitle() {
+        return videoTitle;
+    }
+
+    public void setVideoTitle(String videoTitle) {
+        this.videoTitle = videoTitle;
+    }
+
+    public String getVideoImg() {
+        return videoImg;
+    }
+
+    public void setVideoImg(String videoImg) {
+        this.videoImg = videoImg;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
 }
