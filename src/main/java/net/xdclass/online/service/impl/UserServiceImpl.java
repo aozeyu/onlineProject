d6c8f6e3d898242915e53ce3c310bff91ce61ccb
aozeyu <https://github.com/aozeyu>
2022-04-26 18:03:50 +0800

    随即头像

--- src/main/java/net/xdclass/online/service/impl/UserServiceImpl.java
+++ src/main/java/net/xdclass/online/service/impl/UserServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * @program: online
@@ -36,7 +37,7 @@ public class UserServiceImpl implements UserService {
         if (userInfo.containsKey("phone") && userInfo.containsKey("pwd") && userInfo.containsKey("name")) {
             User user = new User();
             user.setName(userInfo.get("name"));
-            user.setHeadImg("");
+            user.setHeadImg(getRandomImg());
             user.setCreateTime(new Date());
             user.setPhone(userInfo.get("phone"));
             String pwd = userInfo.get("pwd");
@@ -48,10 +49,16 @@ public class UserServiceImpl implements UserService {
         }
     }
     private static final String [] headImg = {
-            "https://xd-video-pc-img.oss-cnbeijing.aliyuncs.com/xdclass_pro/default/head_img/12.jpeg",
-            "https://xd-video-pc-img.oss-cnbeijing.aliyuncs.com/xdclass_pro/default/head_img/11.jpeg",
-            "https://xd-video-pc-img.oss-cnbeijing.aliyuncs.com/xdclass_pro/default/head_img/13.jpeg",
-            "https://xd-video-pc-img.oss-cnbeijing.aliyuncs.com/xdclass_pro/default/head_img/14.jpeg",
-            "https://xd-video-pc-img.oss-cnbeijing.aliyuncs.com/xdclass_pro/default/head_img/15.jpeg"
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/12.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/11.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/13.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/14.jpeg",
+            "https://xd-video-pc-img.oss-cn-beijing.aliyuncs.com/xdclass_pro/default/head_img/15.jpeg"
     };
+    private String getRandomImg() {
+        int size = headImg.length;
+        Random random = new Random();
+        int index = random.nextInt(size);
+        return headImg[index];
+    }
 }
