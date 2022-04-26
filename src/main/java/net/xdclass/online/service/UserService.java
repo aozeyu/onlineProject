c408233cb723ff1819a01b77d90e6c338aee56b5
aozeyu <https://github.com/aozeyu>
2022-04-26 17:24:55 +0800

    登陆注册11

--- src/main/java/net/xdclass/online/service/UserService.java
+++ src/main/java/net/xdclass/online/service/UserService.java
@@ -7,7 +7,7 @@ import java.util.Map;
 
 /**
  * @program: online
- * @description: 用户
+ * @description: 用户登陆服务层
  * @packagename: net.xdclass.online.service
  * @author: 姚泽宇
  * @date: 2022-04-26 17:20
